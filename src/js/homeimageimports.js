4b339f65cc74523c832f82c040f8ce1c3962b957
Tyler Manion <relytmanion@gmail.com>
2020-06-22 17:41:54 -0500

    fixed gallery images

--- src/js/homeimageimports.js
+++ src/js/homeimageimports.js
@@ -44,3 +44,22 @@ const thumbs = document.getElementsByClassName("galleryThumb");
 for (let i = 0; i < thumbs.length; i++) {
 	thumbs[i].src = galleryArray[i];
 }
+import "../img/1.jpg";
+import "../img/2.jpg";
+import "../img/3.jpg";
+import "../img/4.jpg";
+import "../img/5.jpg";
+import "../img/6.jpg";
+import "../img/7.jpg";
+import "../img/8.jpg";
+import "../img/9.jpg";
+import "../img/10.jpg";
+import "../img/11.jpg";
+import "../img/12.jpg";
+import "../img/13.jpg";
+import "../img/14.jpg";
+import "../img/15.jpg";
+import "../img/16.jpg";
+import "../img/17.jpg";
+import "../img/18.jpg";
+import "../img/19.jpg";
