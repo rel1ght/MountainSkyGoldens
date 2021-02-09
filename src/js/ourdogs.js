ffc4ae1b309ca3912ccc3b9538410a870808267b
Tyler Manion <relytmanion@gmail.com>
2021-02-08 20:57:51 -0600

    fixed images

--- src/js/ourdogs.js
+++ src/js/ourdogs.js
@@ -1,11 +1,15 @@
 import "../ourdogs.html";
 import "./sharedimports.js";
-import elliePic from "../img/ellieheadshotcropped.jpg";
-const ellieBioPic = document.getElementById("ellieBioPic");
-ellieBioPic.src = elliePic;
+
 import linaPic from "../img/linaheadshotcropped.jpg";
 const linaBioPic = document.getElementById("linaBioPic");
 linaBioPic.src = linaPic;
+
+import elliePic from "../img/ellieheadshotcropped.jpg";
+const ellieBioPic = document.getElementById("ellieBioPic");
+ellieBioPic.src = elliePic;
+
+
 import dukePic from "../img/dukeheadshotcropped.jpg";
 const dukeBioPic = document.getElementById("dukeBioPic");
 dukeBioPic.src = dukePic;
