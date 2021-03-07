841d14738c80f0ffbef6d65080b4c86ab4ae237d
Tyler Manion <relytmanion@gmail.com>
2021-03-06 23:55:06 -0600

    added venmo

--- src/js/deposit.js
+++ src/js/deposit.js
@@ -1,12 +1,20 @@
 import "../deposit.html";
 import "./sharedimports.js";
 
+import venmoqr from "../img/venmoqr.jpg";
+const venmoPic = document.getElementById("venmo-qr");
+venmoPic.src = venmoqr;
+
 document.getElementById("form").addEventListener("submit", (e) => {
 	e.preventDefault();
 	let paypalContainer = document.getElementById("paypalContainer");
 	paypalContainer.classList.remove("disabled");
 	document.getElementById("pleaseAgree").classList.add("d-none");
 	document.getElementById("pleasePay").classList.remove("d-none");
+	document.getElementById("venmo-qr-container").classList.remove("d-none");
 	paypalContainer.classList.add("depositBorder");
+});
+document.getElementById("venmo-button").addEventListener("click", (e) => {
+	window.open("https://venmo.com/Mountain_Sky_Goldens", "_blank");
 });
 document.getElementById("dateInput").valueAsDate = new Date();
--- src/js/ourdogs.js
+++ src/js/ourdogs.js
@@ -9,10 +9,13 @@ import elliePic from "../img/ellieheadshotcropped.jpg";
 const ellieBioPic = document.getElementById("ellieBioPic");
 ellieBioPic.src = elliePic;
 
-
 import dukePic from "../img/dukeheadshotcropped.jpg";
 const dukeBioPic = document.getElementById("dukeBioPic");
 dukeBioPic.src = dukePic;
+
+import magnumPic from "../img/magnumheadshotcropped.jpg";
+const magnumBioPic = document.getElementById("magnumBioPic");
+magnumBioPic.src = magnumPic;
 // import puppyTemp from "../img/temppuppythumb.jpg";
 // const puppyThumb = document.getElementById("puppyTemp");
 // puppyThumb.src = puppyTemp;
